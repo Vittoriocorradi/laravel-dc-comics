12ec01379c166ce561a7f3088acb09ebf0570279
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-10 14:03:01 +0200

    Milestone 1/Model e Migration

---
+++ database/migrations/2023_05_10_114650_create_comics_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb');
+            $table->float('price', 5, 2)->unsigned();
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 25);
+            $table->string('artists', 25);
+            $table->string('writers', 25);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
